7f706c78c87dd99266f07e71e8a8d724b9165557
JGeun <68798525+JGeun@users.noreply.github.com>
2022-10-11 17:22:01 +0900

    Set Setting Fragment  UI & UX touch event
    * Set Modify Profile UI
    
    * Modify Setting Fragment UX touch event

---
+++ setting/src/main/java/com/soma/lof/setting/ModifyProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.soma.lof.setting
+
+import com.soma.common.base.BaseFragment
+import com.soma.lof.setting.databinding.FragmentProfileModifyBinding
+
+class ModifyProfileFragment : BaseFragment<FragmentProfileModifyBinding>(R.layout.fragment_profile_modify) {
+    override fun initView() {
+        binding.modifyProfileImage.setOnClickListener {
+
+        }
+    }
+}
--- setting/src/main/java/com/soma/lof/setting/SettingFragment.kt
+++ setting/src/main/java/com/soma/lof/setting/SettingFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.soma.common.base.BaseFragment
 import com.soma.lof.setting.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +18,10 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
     private val viewModel by viewModels<SettingViewModel>()
 
     override fun initView() {
+        binding.settingProfileArea.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_modifyProfileFragment)
+        }
+
         binding.settingLogoutArea.setOnClickListener {
             viewModel.signOut(requireActivity())
         }
