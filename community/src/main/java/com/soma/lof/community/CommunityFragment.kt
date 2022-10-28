15828c09720bbf555e6f9f3caf9c167d6a1d0ccf
JGeun <pjk8926@gmail.com>
2022-10-28 16:19:20 +0900

    Set CommunityFragment UI

--- community/src/main/java/com/soma/lof/community/CommunityFragment.kt
+++ community/src/main/java/com/soma/lof/community/CommunityFragment.kt
@@ -1,11 +1,24 @@
 package com.soma.lof.community
 
-import com.soma.lof.community.databinding.FragmentCommunityBinding
+import androidx.fragment.app.viewModels
 import com.soma.common.ui.base.BaseFragment
+import com.soma.lof.community.databinding.FragmentCommunityBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community) {
 
+    private val viewModel by viewModels<CommunityViewModel>()
+    private lateinit var postAdapter: PostAdapter
+
     override fun initView() {
+        postAdapter = PostAdapter()
+        binding.communityRv.addItemDecoration(PostItemDecoration())
+        bind {
+            adapter = postAdapter
+        }
+
+        postAdapter.submitList(viewModel.dummy)
     }
 
     companion object {
